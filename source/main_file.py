3c95590b4b08a76f5e29d6603725775aedf77263
WazeAzure <55005873+WazeAzure@users.noreply.github.com>
2023-07-24 22:57:06 +0700

    Update: import library in supporting_file.py and create basic reader code in main_file.py

--- source/main_file.py
+++ source/main_file.py
@@ -1,8 +1,37 @@
-import supporting_file
+from supporting_file import *
 
 def main():
-    #main code
-    return None
+    # main code
+    
+    cap = cv2.VideoCapture(0)
+    while True:
+        _, frame = cap.read()
+        height, width, _ = frame.shape
+        
+        # change BGR to HSV color format
+        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        frame = cv2.flip(frame, 1) # flip frames
+
+        # get center coordinate (x, y) of window
+        cx = int(width / 2)
+        cy = int(height / 2)
+        print(cx, cy)
+        
+        # pick pixel value
+        pixel_center = hsv_frame(cy, cx)
+        print(f"pixel center - {pixel_center}")
+        # add square
+        cv2.square(frame, (cx, cy), 5, (255, 0, 0), 3)
+        
+        # show video per frame
+        cv2.imshow("Frame", frame)
+
+        key = cv2.waitKey(1)
+        if key == 27: # press 'esc'
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
     
     
 
--- source/supporting_file.py
+++ source/supporting_file.py
@@ -1,5 +1,10 @@
-import numpy as np
 import cv2
+import numpy as np
 """
     untuk import file dan bahan2 atau variabel2 yang akan digunain
+    
+    File untuk import color codes, etc. 
+    All imports Library are in main_file.py!
 """
+
+print("hello")
