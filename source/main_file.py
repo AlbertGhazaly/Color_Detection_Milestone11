885d1337c9f462b2896cf45660210db105bcd4f1
Albert Ghazaly <albert260306@gmail.com>
2023-07-24 20:56:36 +0700

    build file structure

---
+++ source/main_file.py
@@ -0,0 +1,10 @@
+import supporting_file
+
+def main():
+    #main code
+    return None
+    
+    
+
+if __name__ == "__main__":
+    main()
---
+++ source/supporting_file.py
@@ -0,0 +1,5 @@
+import numpy as np
+import cv2
+"""
+    untuk import file dan bahan2 atau variabel2 yang akan digunain
+"""
