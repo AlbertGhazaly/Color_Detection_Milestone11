7f267257dda3060884712aef2e7a931779c0d27c
trimonuter <mrqaist@gmail.com>
2023-08-01 17:28:30 +0700

    added color label below video

--- bin/app.py
+++ bin/app.py
@@ -40,20 +40,27 @@ def updateFrame():
 
         # add text
         cv2.putText(frame, color, (10, 50), 0, 1, (b, g, r), 2)
+        colorLabel.config(text=color)
         
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert from BGR to RGB
         image = Image.fromarray(frame)  # Create an Image object from the frame
         photo = ImageTk.PhotoImage(image=image)  # Create a PhotoImage from the Image object
-        label.config(image=photo)  # Update the label with the new image
-        label.image = photo  # Keep a reference to the PhotoImage to prevent garbage collection
+        videoLabel.config(image=photo)  # Update the videoLabel with the new image
+        videoLabel.image = photo  # Keep a reference to the PhotoImage to prevent garbage collection
     
     root.after(10, updateFrame) # Rerun updateFrame function after 10 miliseconds
 
 root = tk.Tk()
-root.title("Webcam Display")
+root.title("Color Detection App")
 
-label = tk.Label(root)
-label.pack()
+videoLabel = tk.Label(root)
+videoLabel.grid(row=0, column=0)
+
+colorLabel = tk.Label(root, text="Color")
+colorLabel.grid(row=1, column=0)
+
+entry = tk.Entry(root)
+entry.grid(row=1, column=1)
 
 webcam = cv2.VideoCapture(0)  # 0 for the default camera (change to 1 for an external camera)
 updateFrame()
