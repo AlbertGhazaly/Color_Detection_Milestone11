2161975ef31a9e9fbfa8da62f24e3619dff325e8
louisferdyo <louisferdyo1028@gmail.com>
2023-08-02 12:20:52 +0700

    Update help_page.py

--- tkinter_app/help_page.py
+++ tkinter_app/help_page.py
@@ -1,10 +1,22 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
-from tkinter import Canvas
 
 class About(tk.Tk):
     def __init__(self):
         super().__init__()
 
-        self.canvas = Canvas
+        # tambahin text didalam sini
+        self.geometry("1200x900")
+        self.title("tes")
+         # tombol
+        self.button = ttk.Button(self, text='Click Me')
+        self.button['command'] = self.button_clicked
+        self.button.pack()
+
+    # bisa juga bikin fungsi baru.
+    def button_clicked(self):
+        showinfo(title="information", message="hello")
+
+app = About()
+app.mainloop()
