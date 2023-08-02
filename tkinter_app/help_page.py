642ca3333d030b9b8e782ac6e2048a9d09c7fbed
louisferdyo <louisferdyo1028@gmail.com>
2023-08-02 16:18:52 +0700

    Update help_page.py

--- tkinter_app/help_page.py
+++ tkinter_app/help_page.py
@@ -31,14 +31,22 @@ class ResizingCanvas(Canvas):
 
 def on_back_click(event):
     print("Back button clicked!") # Replace this function with the desired action when the "Back" button is clicked
+    
+def on_back_enter(event):
+    # Change the mouse cursor to the hand icon when hovering over the "Back" button
+    mycanvas.config(cursor="hand2")
+def on_back_leave(event):
+    # Change the mouse cursor back to the default arrow when leaving the "Back" button
+    mycanvas.config(cursor="arrow")
 
 def main():
+    global mycanvas
     root = Tk()
     myframe = Frame(root)
     myframe.pack(fill=BOTH, expand=YES)
     mycanvas = ResizingCanvas(myframe, width=720, height=540, bg="white", highlightthickness=0)
     mycanvas.pack(fill=BOTH, expand=YES)
-
+    
     # add some widgets to the canvas
     mycanvas.create_rectangle(0, 0, 720, 67, fill="#D9D9D9", outline="#D9D9D9")
     mycanvas.create_text(360, 30, text="Help", fill="#000000", font='Arial 24', tags="title_tag")
@@ -51,13 +59,15 @@ def main():
     mycanvas.create_rectangle(26, 440, 131, 480, fill="#D9D9D9", outline="#D9D9D9", tags=["back_button"])
     mycanvas.create_text(78, 460, text="Back", fill="#000000", font='Arial 16', tags=["button_tag", "back_button"])
     mycanvas.create_rectangle(0, 494, 720, 540, fill="#D9D9D9", outline="#D9D9D9")
-    mycanvas.create_text(365, 515, text="Created By ....", fill="#000000", font='Arial 16', tags="text_tag")
+    mycanvas.create_text(365, 515, text="Created By ....", fill="#000000", font='Arial 12', tags="text_tag")
     mycanvas.pack()
     # tag all of the drawn widgets
     mycanvas.addtag_all("all")
 
     # Bind the back button to the on_back_click function
     mycanvas.tag_bind("back_button", "<Button-1>", on_back_click)
+    mycanvas.tag_bind("back_button", "<Enter>", on_back_enter)
+    mycanvas.tag_bind("back_button", "<Leave>", on_back_leave)
 
     root.mainloop()
 
