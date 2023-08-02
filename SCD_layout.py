8c1ba14401863f3618745e0527cbe5884e245703
WazeAzure <bryan.rich0604@gmail.com>
2023-08-02 17:33:26 +0700

    Update: Move files to root folder

--- SCD_layout.py
+++ SCD_layout.py
@@ -16,28 +16,22 @@ class frame :
         self.root.title("SCD layout")
         self.root.config(bg="white")
         self.root.maxsize(1200, 900)
+        
+        self.main()
 
-
-# frame warna
+    def main(self):
+        # frame warna
         
         self.frame_warna =tk.Frame(self.root, width=70, height=70, bg="Blue")
         self.frame_warna.grid(row=0, column=0, padx=50, pady=10)
+
         self.nama_warna = tk.Frame(self.root,width=50,height=70,bg="White" ).grid(row=0,column=1,padx=0,pady=0)
         tk.Label(self.nama_warna, text="Hex",bg="White").grid(row=0,column=1,padx=0,pady=10,sticky="SW")
         tk.Label(self.nama_warna, text="Blue", bg="White").grid(row=0,column=1,padx=0,pady=10,sticky="NW")
+
         self.mode= tk.Frame(self.root,width=70,height=70,bg="White").grid(row = 0,column= 2,padx=600,pady=10,sticky="NE")
         tk.Label(self.mode,text="Single Color Detection" ).grid(row = 0,column= 2,padx=5,pady=10)
         
-        
-        
         self.root.mainloop()
-        
-        
-        
-    
-        
-    
-
-        
         
 frame()
