def96ad5467aa081aa3be81f598f8e18bd2edcd7
MickyV18 <mickyvalentino18@gmail.com>
2023-08-02 12:04:18 +0700

    bikin rectangle

--- tkinter_app/start_page.py
+++ tkinter_app/start_page.py
@@ -1,13 +1,20 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showinfo
+from tkinter import Canvas
 
 class Start(tk.Tk):
     def __init__(self):
         super().__init__()
 
         # tambahin text didalam sini
-        
+        self.canvas = Canvas(width = 1200, height = 900, bg = "white")
+        self.canvas.create_rectangle(0,0,1200,112, fill ='#D9D9D9', outline='#D9D9D9')
+        self.canvas.create_rectangle(0,900,1200,823, fill ='#D9D9D9', outline='#D9D9D9')
+        self.canvas.pack()
+
+        self.geometry("1200x900")
+        self.title ("Color Detection App")
         # tombol
         self.button = ttk.Button(self, text='Click Me')
         self.button['command'] = self.button_clicked
