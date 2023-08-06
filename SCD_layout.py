70b4345a098028fb3a93c323c06b5c84ebaeea40
TheoLN <theoliviusnatael@gmail.com>
2023-08-06 19:14:03 +0700

    commit scd layout

--- SCD_layout.py
+++ SCD_layout.py
@@ -22,15 +22,15 @@ class SCD():
     def main(self):
         # frame warna
         
-        self.frame_warna =tk.Frame(self.root, width=70, height=70, bg="Blue")
-        self.frame_warna.grid(row=0, column=0, padx=50, pady=10)
+        self.frame_warna =tk.Frame(self.root, width=100, height=70, bg="Blue")
+        self.frame_warna.pack(side=tk.LEFT, anchor="nw",padx=100)
+        
+        self.nama_warna = tk.Frame(self.root,width=50,height=70,bg="White" ).pack(side=tk.LEFT)
+        tk.Label(self.nama_warna, text="Hex",bg="White").pack(anchor="w")
+        tk.Label(self.nama_warna, text="Blue", bg="White").pack(anchor ="w")
 
-        self.nama_warna = tk.Frame(self.root,width=50,height=70,bg="White" ).grid(row=0,column=1,padx=0,pady=0)
-        tk.Label(self.nama_warna, text="Hex",bg="White").grid(row=0,column=1,padx=0,pady=10,sticky="SW")
-        tk.Label(self.nama_warna, text="Blue", bg="White").grid(row=0,column=1,padx=0,pady=10,sticky="NW")
-
-        self.mode= tk.Frame(self.root,width=70,height=70,bg="White").grid(row = 0,column= 2,padx=600,pady=10,sticky="NE")
-        tk.Label(self.mode,text="Single Color Detection" ).grid(row = 0,column= 2,padx=5,pady=10)
+        self.mode= tk.Frame(self.root,width=70,height=70,bg="White").pack( side=tk.RIGHT,anchor="ne")
+        tk.Label(self.mode,text="Single Color Detection",justify="left" ).pack(side=tk.RIGHT,anchor="n")
         
         self.root.mainloop()
 
