70217d7e0ea2dc9fee7061fcadf1440e5cdfd0f3
Albert Ghazaly <albert260306@gmail.com>
2023-08-07 09:57:45 +0700

    adjustment

--- color_detection_file/supporting_file.py
+++ color_detection_file/supporting_file.py
@@ -1,40 +1,3 @@
-<<<<<<< HEAD:source/supporting_file.py
-import cv2
-import numpy as np
-"""
-    untuk import file dan bahan2 atau variabel2 yang akan digunain
-    
-"""
-def median(list1,list2):
-    list = []
-    for i in range(len(list1)):
-        list.append((list1[i]+list2[i])/2)
-    return list
-
-color_list = ["black","white","red","Red","green","blue","yellow","purple","orange","gray"]
-color_dict_HSV = {'black': [[180, 255, 30], [0, 0, 0]],
-              'white': [[180, 18, 255], [0, 0, 231]],
-              'red': [[180, 255, 255], [159, 50, 70]],
-              'Red': [[9, 255, 255], [0, 50, 70]],
-              'green': [[89, 255, 255], [36, 50, 70]],
-              'blue': [[128, 255, 255], [90, 50, 70]],
-              'yellow': [[35, 255, 255], [25, 50, 70]],
-              'purple': [[158, 255, 255], [129, 50, 70]],
-              'orange': [[24, 255, 255], [10, 50, 70]],
-              'gray': [[180, 18, 230], [0, 0, 40]]}
-def color_decider(pixel):
-    hue_value = pixel[0]
-    saturation_value = pixel[1]
-    light_value = pixel[2]
-    color = "undefined"
-    for colour in color_dict_HSV:
-        if ((color_dict_HSV[colour][1][0]<= hue_value and color_dict_HSV[colour][0][0]>= hue_value) and 
-            (color_dict_HSV[colour][1][1]<= saturation_value and color_dict_HSV[colour][0][1]>=saturation_value) and
-            (color_dict_HSV[colour][1][2]<= light_value and color_dict_HSV[colour][0][2]>=light_value)):
-            color = colour
-
-    return color
-=======
 import cv2
 import numpy as np
 # color dictionary HSV (upper range, lower range)
@@ -62,4 +25,3 @@ def color_decider(pixel):
             color = colour
             
     return color
->>>>>>> main:color_detection_file/supporting_file.py
