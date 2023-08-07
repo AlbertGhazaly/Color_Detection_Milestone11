fac5288dc1f5aaf32c74283f994612fbec4db536
trimonuter <mrqaist@gmail.com>
2023-08-07 20:08:48 +0700

    added color detection v2 to camera_page

--- camera_page.py
+++ camera_page.py
@@ -33,9 +33,19 @@ class App(tk.Frame):
         
         # Select detection mode
         yMode = 16
+        self.onSingleMode = True
         tk.Label(self, text='Mode:', font='Aerial 14 bold').place(x=528, y= yMode + 7)
-        tk.Label(self,text="Single Color").place(x=600, y= yMode)
-        tk.Label(self,text="Detection").place(x=600, y= yMode + 27)
+        singleColor = tk.Label(self,text="Single Color")
+        singleColor.place(x=600, y= yMode)
+        
+        detection = tk.Label(self,text="Detection")
+        detection.place(x=600, y= yMode + 27)
+        
+        def toggleMode(event):
+            self.onSingleMode = not self.onSingleMode
+        
+        singleColor.bind('<Button-1>', toggleMode)
+        detection.bind('<Button-1>', toggleMode)
         
         # Camera Section
         self.cameraTop = 110
@@ -97,8 +107,10 @@ class App(tk.Frame):
         self.main()
             
     def main(self):
-        print("Main is running")
-        self.updateFrame()
+        if self.onSingleMode:
+            self.updateFrame()
+        else:
+            self.updateFrameV2()
     
     def updateFrame(self):
         if self.cameraRunning:
@@ -164,7 +176,116 @@ class App(tk.Frame):
                 self.canvas.itemconfig(self.colorLabel, text=color)
                 self.canvas.itemconfig(self.colorHex, text = hex)
                 
-                self.after(10, self.updateFrame) # Rerun updateFrame function after 10 miliseconds
+                self.after(10, self.main) # Rerun updateFrame function after 10 miliseconds
+
+    def updateFrameV2(self):
+        if self.cameraRunning:
+            # Reading the video from the
+            # webcam in image frames
+            _, frame = self.webcam.read()
+
+            # Convert the frame in
+            # BGR(RGB color space) to
+            # HSV(hue-saturation-value)
+            # color space
+            hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+            frame = cv2.flip(frame, 1) # flip frames
+
+            # define mask for each color using range of color
+            # mask = cv2.inRange(frame, lower range, upper range)
+            black_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["black"][1], np.uint8), np.array(color_dict_HSV["black"][0], np.uint8))
+            white_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["white"][1], np.uint8), np.array(color_dict_HSV["white"][0], np.uint8))
+            red_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["red"][1], np.uint8), np.array(color_dict_HSV["red"][0], np.uint8))
+            Red_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["Red"][1], np.uint8), np.array(color_dict_HSV["Red"][0], np.uint8))
+            green_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["green"][1], np.uint8), np.array(color_dict_HSV["green"][0], np.uint8))
+            blue_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["blue"][1], np.uint8), np.array(color_dict_HSV["blue"][0], np.uint8))
+            yellow_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["yellow"][1], np.uint8), np.array(color_dict_HSV["yellow"][0], np.uint8))
+            purple_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["purple"][1], np.uint8), np.array(color_dict_HSV["purple"][0], np.uint8))
+            orange_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["orange"][1], np.uint8), np.array(color_dict_HSV["orange"][0], np.uint8))
+            brown_mask = cv2.inRange(hsvFrame, np.array(color_dict_HSV["brown"][1], np.uint8), np.array(color_dict_HSV["brown"][0], np.uint8))
+            
+
+
+            # Morphological Transform, Dilation
+            # for each color and bitwise_and operator
+            # between frame and mask determines
+            # to detect only that particular color
+            kernel = np.ones((5, 5), "uint8")
+
+            #Create mask and implementation for each color in image
+
+            # For black color
+            black_mask = cv2.dilate(black_mask, kernel)
+            res_black = cv2.bitwise_and(frame, frame,
+                                    mask = black_mask)
+            # For white color
+            white_mask = cv2.dilate(white_mask, kernel)
+            res_white = cv2.bitwise_and(frame, frame,
+                                    mask = white_mask)
+            # For red color
+            red_mask = cv2.dilate(red_mask, kernel)
+            res_red = cv2.bitwise_and(frame, frame,
+                                    mask = red_mask)
+            # For Red color
+            Red_mask = cv2.dilate(Red_mask, kernel)
+            res_Red = cv2.bitwise_and(frame, frame,
+                                    mask = Red_mask)
+            # For green color
+            green_mask = cv2.dilate(green_mask, kernel)
+            res_green = cv2.bitwise_and(frame, frame,
+                                        mask = green_mask)
+            # For blue color
+            blue_mask = cv2.dilate(blue_mask, kernel)
+            res_blue = cv2.bitwise_and(frame, frame,
+                                    mask = blue_mask)
+            # For yellow color
+            yellow_mask = cv2.dilate(yellow_mask, kernel)
+            res_yellow = cv2.bitwise_and(frame, frame,
+                                    mask = yellow_mask)
+            # For purple color
+            purple_mask = cv2.dilate(purple_mask, kernel)
+            res_purple = cv2.bitwise_and(frame, frame,
+                                    mask = purple_mask)
+            # For blue color
+            orange_mask = cv2.dilate(orange_mask, kernel)
+            res_orange = cv2.bitwise_and(frame, frame,
+                                    mask = orange_mask)
+            # For brown color
+            brown_mask = cv2.dilate(brown_mask, kernel)
+            res_brown = cv2.bitwise_and(frame, frame,
+                                    mask = brown_mask)
+            
+
+            # list of colors
+            color_mask_list = [black_mask,white_mask,red_mask,Red_mask,green_mask,blue_mask,yellow_mask,
+                    purple_mask,orange_mask,brown_mask]
+
+            for order in range(0,10):
+                # Creating contour to track each color
+                contours, hierarchy = cv2.findContours(color_mask_list[order],
+                                                    cv2.RETR_TREE,
+                                                    cv2.CHAIN_APPROX_SIMPLE)
+
+                for pic, contour in enumerate(contours):
+                    area = cv2.contourArea(contour)
+                    if(area > 1500): #Size of color detected
+                        x, y, w, h = cv2.boundingRect(contour) #the rectangle
+                        frame = cv2.rectangle(frame, (x, y),
+                                                (x + w, y + h),
+                                                tuple(color_dict_BGR[color_list[order]]), 2)
+                        #Puting text on rectangle
+                        cv2.putText(frame, color_list[order], (x, y),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+                                    tuple(color_dict_BGR[color_list[order]]))		
+                        
+            frame = cv2.resize(frame, (433, 325))
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # Convert from BGR to RGB
+            image = Image.fromarray(frame)  # Create an Image object from the frame
+            photo = ImageTk.PhotoImage(image=image)  # Create a PhotoImage from the Image object
+            self.videoLabel.config(image=photo)  # Update the videoLabel with the new image
+            self.videoLabel.image = photo  # Keep a reference to the PhotoImage to prevent garbage collection
+            
+            self.after(10, self.main) # Rerun updateFrame function after 10 miliseconds
 
     def backButton(self, event):
         self.show_page_callback('page2')
